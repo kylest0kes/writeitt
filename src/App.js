e8ea7596389c5a765e4b637719a7646ef079aaa4
kylest0kes <kylestokes1551@gmail.com>
2024-06-13 18:43:58 -0400

    set up the start of routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.scss';
-import Header from './Components/Header/Header';
+import TheRoutes from './Routes';
 
 function App() {
   return (
     <div>
-      <Header />
+      <TheRoutes />
     </div>
   );
 }
