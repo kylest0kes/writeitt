fb443ed6c429b7954b8b7f0c1c56c1834129e9d6
kylest0kes <kylestokes1551@gmail.com>
2024-06-13 21:13:59 -0400

    added functionality so that the modals close if a user clicks outside of them

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useRef, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import './Modal.scss'
 
 const Modal = ({ isOpen, onClose, children }) => {
+    const modalRef = useRef();
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (modalRef.current && !modalRef.current.contains(event.target)) {
+                onClose();
+            }
+        };
+
+        if (isOpen) {
+            document.addEventListener('mousedown', handleOutsideClick);
+        } else {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [isOpen, onClose]);
+
     if (!isOpen) return null;
 
     return ReactDOM.createPortal(
         <div className="modal-overlay">
-            <div className="modal">
+            <div className="modal" ref={modalRef}>
                 <FontAwesomeIcon icon={faCircleXmark} onClick={onClose} className='closeBtn'/>
                 {children}
             </div>
