7c9f1777a07fdf5e7bb50d57b3f92b5ee4ea743f
kylest0kes <kylestokes1551@gmail.com>
2024-06-14 21:20:02 -0400

    set up form submission when sign up form completed, store all the values in state, to be used later

--- server/client/src/Components/SignUp/SignUp.js
+++ server/client/src/Components/SignUp/SignUp.js
@@ -1,33 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUp.scss";
 
 function SignUp() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '', 
+    password: '',
+    confirmPassword: ''
+  });
+
+  const handleSetFormData = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault() 
+    console.log('data: ', formData)
+  };
+
+
   return (
     <div id="signup-wrapper">
-      <form action="#" method="post">
+      <form action="#" method="post" onSubmit={handleFormSubmit}>
         <h1 className="heading">Create Your Account</h1>
 
         <p>
           <label for="uname">Username:</label>
-          <input id="uname" type="text" name="user_name" />
+          <input id="uname" type="text" name="username" onChange={handleSetFormData} value={formData.username} />
         </p>
 
         <p>
           <label for="mail">Email:</label>
-          <input id="mail" type="text" name="email" />
+          <input id="mail" type="email" name="email" onChange={handleSetFormData} value={formData.email} />
         </p>
 
         <p>
-          <label for="pword">Password:</label>
-          <input id="pword" type="password" name="enter_password" />
+          <label for="password">Password:</label>
+          <input id="password" type="password" name="password" onChange={handleSetFormData} value={formData.password} />
         </p>
 
         <p>
-          <label for="rpword">Confirm Password:</label>
-          <input id="rpword" type="password" name="confirm_password" />
+          <label for="confirmPassword">Confirm Password:</label>
+          <input id="confirmPassword" type="password" name="confirmPassword" onChange={handleSetFormData} value={formData.confirmPassword} />
         </p>
 
-        <input type="submit" value="Submit" id="submit" />
+        <button type="submit" id="submit" className="signup-submit-btn">Submit</button>
       </form>
     </div>
   );
