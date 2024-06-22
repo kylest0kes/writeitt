d4fc65148fb36e5b422f14186eab95a45736e96f
kylest0kes <kylestokes1551@gmail.com>
2024-06-22 16:56:27 -0400

    created AuthContext

---
+++ server/client/src/Contexts/AuthContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from 'react'
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+
+    const [authToken, setAuthToken] = useState(localStorage.getItem('authToken'));
+
+    const login = (token) => {
+        setAuthToken(token);
+        localStorage.setItem('authToken', token);
+    }
+
+    const logout  = () => {
+        setAuthToken(null);
+        localStorage.removeItem('authToken');
+    }
+
+    return (
+        <AuthContext.Provider value={{ authToken, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
