7bae9287e2cee7569f6e8c11cd775e40a5d41bd6
kylest0kes <kylestokes1551@gmail.com>
2024-09-23 19:04:19 -0400

    implemented hiding and showing of list items under the Library tab, and other style fixes

--- server/client/src/Components/LeftSideMenu/LeftSideMenu.js
+++ server/client/src/Components/LeftSideMenu/LeftSideMenu.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './LeftSideMenu.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBuildingColumns, faHouse, faPenToSquare, faTableList } from '@fortawesome/free-solid-svg-icons';
+import { faBuildingColumns, faChevronDown, faChevronUp, faHouse, faPenToSquare, faTableList } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 function LeftSideMenu() {
+  const navigate = useNavigate();
+  const [isLibraryOpen, setIsLibraryOpen] = useState(true);
+
+  const handleHomeClick = () => {
+    navigate("/");
+  }
+
+  const handleLibraryDropdownClick = () => {
+    setIsLibraryOpen(!isLibraryOpen);
+  }
+
   return (
     <div className="left-menu">
        <ul className="sidebar-menu">
         <li className="menu-item">
-          <a href="#home">
+          <div className="home-link" onClick={handleHomeClick}>
             <FontAwesomeIcon className="icon home-icon" icon={faHouse}/>
             <span>Home</span>
-          </a>
+          </div>
         </li>
 
         <li className="menu-item">
@@ -23,17 +35,28 @@ function LeftSideMenu() {
         </li>
       </ul>
 
+      <hr />
+
       <ul className="sidebar-section">
-        <li className="section-item">
-          <span>Library</span>
-          <FontAwesomeIcon className="icon dropdown-icon" icon={faBuildingColumns} />
-        </li>
-        <li className="menu-item">
-          <a href="#create-story">
-            <FontAwesomeIcon className="icon create-community-icon" icon={faPenToSquare} />
-            <span>Start A Story...</span>
-          </a>
-        </li>
+        { isLibraryOpen ? (
+          <div>
+            <li className="section-item library" onClick={handleLibraryDropdownClick}>
+              <span>Library</span>
+              <FontAwesomeIcon className="icon dropdown-icon" icon={faChevronUp} />
+            </li>
+            <li className="menu-item">
+              <div>
+                <FontAwesomeIcon className="icon create-community-icon" icon={faPenToSquare} />
+                <span>Start A Story...</span>
+              </div>
+            </li>
+          </div>
+        ) : (
+          <li className="section-item library" onClick={handleLibraryDropdownClick}>
+            <span>Library</span>
+            <FontAwesomeIcon className="icon dropdown-icon" icon={faChevronDown} />
+          </li>
+        )}
       </ul>
 
     </div>
