e90c0eb75ed43632f18df177bee38cae2a207251
kylest0kes <kylestokes1551@gmail.com>
2024-10-26 19:17:13 -0400

    styled and laid out the items in the edit story menu

--- server/client/src/Components/EditStoryMenu/EditStoryMenu.js
+++ server/client/src/Components/EditStoryMenu/EditStoryMenu.js
@@ -5,7 +5,18 @@ import './EditStoryMenu.scss';
 const EditStoryMenu = forwardRef((props, ref) => {
   return (
     <div className='edit-story-menu'>
-        EditStoryMenu
+        <div className='edit-story-menu-nav'>
+            <div className='edit-story-menu-nav-item'>
+                <span className='edit-story-menu-nav-item-span'>
+                    Edit Story
+                </span>
+            </div>
+            <div className='edit-story-menu-nav-item delete-story'>
+                <span className='edit-story-menu-nav-item-span'>
+                    Delete Story
+                </span>
+            </div>
+        </div>
     </div>
   )
 })
